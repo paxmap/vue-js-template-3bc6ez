b63724a2a41aec2ac529895c01554ef25bdbbc02
paxmap <paxton@turnbullthomson.com>
2020-07-15 15:40:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-js-template-3bc6ez
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-js-template-3bc6ez)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+import Vue from 'vue';
+
+const vue = new Vue({
+  el: '#app',
+  data: {
+    title: 'Vue J Template!'
+  }
+});
